3d5dcdb13ea4ed8ae50c69d404387f38ae90371f
Juan Carlos <jc.campbellg@gmail.com>
2025-01-03 18:30:26 -0600

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,10 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
-
+  const key = searchParams.get('key')
+  if (key !== process.env.TOKEN) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+  }
 }
 
 // See "Matching Paths" below to learn more
