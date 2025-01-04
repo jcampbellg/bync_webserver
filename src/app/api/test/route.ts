5f0bb25064da49b642574771d3c5e9ebb7faf5a9
Juan Carlos <jc.campbellg@gmail.com>
2025-01-03 19:06:59 -0600

    File upload test working

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -2,7 +2,8 @@ export const dynamic = 'force-dynamic'
 
 import { Storage } from '@google-cloud/storage'
 import fs from 'fs'
-import { NextResponse } from 'next/server'
+import { fileTypeFromBuffer } from 'file-type'
+import { NextRequest, NextResponse } from 'next/server'
 
 const projectId = process.env.PROJECT_ID || ''
 const bucketName = process.env.BUCKET_NAME || ''
@@ -16,26 +17,55 @@ const storage = new Storage({
   keyFilename: './service-account.json'
 })
 
-export async function GET() {
-  try {
-    // Get a reference to the specified bucket
-    const bucket = storage.bucket(bucketName)
+const bucket = storage.bucket(bucketName)
 
-    // Upload the specified file to the bucket with the given destination name
-    const filePath = 'hi.txt'
-    const ret = await bucket.upload(filePath, {
-      destination: filePath
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const filePath = searchParams.get('file')
+
+  if (!filePath) {
+    return NextResponse.json({ message: 'No file specified' }, { status: 400 })
+  }
+
+  try {
+    const file = bucket.file(filePath)
+
+    await file.download({
+      destination: `./${filePath}`
     })
 
-    // Return the result of the upload operation
-    // return NextResponse.json({ message: 'File uploaded successfully', data: ret })
+    // Get Buffer from destination file
+    const fileData = fs.readFileSync(`./${filePath}`)
+    const type = await fileTypeFromBuffer(fileData)
 
-    // get file from bucket and return the file itself
-    const file = bucket.file(filePath)
-    const fileData = await file.download()
-    const response = new NextResponse(fileData)
-    response.headers.set('content-type', file.metadata.contentType)
+    const response = new Response(fileData)
+    // @ts-expect-error
+    response.headers.set('content-type', type?.mime)
+
+    // Delete the downloaded file
+    fs.unlinkSync(`./${filePath}`)
+
     return response
+  } catch (error) {
+    return NextResponse.json({ message: 'Error downloading file', error }, { status: 500 })
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const formData = await request.formData()
+    const uploadFile = formData.get('file') as File
+    const arrayBuffer = await uploadFile.arrayBuffer()
+    const buffer = Buffer.from(arrayBuffer)
+
+    // Upload the specified file to the bucket with the given destination name
+    const file = bucket.file(uploadFile.name)
+
+    await file.save(buffer, {
+      contentType: uploadFile.type
+    })
+
+    return NextResponse.json({ message: 'File uploaded successfully', file: uploadFile.name, contentType: uploadFile.type })
   } catch (error) {
     return NextResponse.json({ message: 'Error uploading file', error }, { status: 500 })
   }
