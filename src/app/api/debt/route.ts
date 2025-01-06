625dc07040cd66dd68ab40e1ea39fb1514ae1193
Juan Carlos <jc.campbellg@gmail.com>
2025-01-05 20:51:10 -0600

    transactions and debts POST

--- src/app/api/debt/route.ts
+++ src/app/api/debt/route.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 import { amountValidation, descriptionValidation, notesValidation, symbolValidation } from '@/utils/validation'
 import { NextRequest, NextResponse } from 'next/server'
 import db from '@/utils/db'
-import { Payment, Transaction } from '@/utils/dbTypes'
+import { Debt, Transaction } from '@/utils/dbTypes'
 
 export async function POST(request: NextRequest) {
   const body = await request.json().then((data) => data).catch(() => null)
@@ -13,20 +13,13 @@ export async function POST(request: NextRequest) {
     notes: notesValidation,
     amount: amountValidation,
     currency: symbolValidation,
-    accountId: z.number().int(),
     groupId: z.number().int().optional(),
     categoryId: z.number().int().optional(),
-    paidAt: z.string().datetime()
+    dueAt: z.string().datetime()
   }).safeParse(body)
 
   if (!!input.error) {
     return NextResponse.json({ error: input.error }, { status: 400 })
-  }
-
-  const accountExists = await db('accounts').where('id', input.data.accountId)
-
-  if (accountExists.length === 0) {
-    return NextResponse.json({ error: 'Account does not exist' }, { status: 400 })
   }
 
   if (input.data.groupId) {
@@ -48,15 +41,16 @@ export async function POST(request: NextRequest) {
   const transaction = await db<Transaction>('transactions').insert({
     description: input.data.description,
     notes: input.data.notes,
-    isPaid: true,
+    isPaid: false,
+    isDebt: true,
+    isPending: true
   }).returning('*').then(t => t[0])
 
-  await db<Payment>('payments').insert({
+  await db<Debt>('debts').insert({
     transactionId: transaction.id,
-    accountId: input.data.accountId,
     currency: input.data.currency,
     amount: input.data.amount,
-    paidAt: input.data.paidAt
+    dueAt: input.data.dueAt
   })
 
   return NextResponse.json<Transaction>(transaction, { status: 200 })
